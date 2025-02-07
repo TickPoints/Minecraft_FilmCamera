d9bcfe23f0e7dc2129430004b99b2c9f2ae50e96
TickPoints <tickpoints@outlook.com>
2025-02-07 09:30:35 +0800

    Updated data initialization.

--- scripts/controller/editor_monitor/data_initialize.js
+++ scripts/controller/editor_monitor/data_initialize.js
@@ -1,16 +1,20 @@
 import {
     writeWorldDataInit,
     writePlayerDataInit,
-    getDataManager
+    addDataPreprocessor
 }
 from "../../lib/data_manager.js";
 
+const SYSTEM_VERSION = [1, 0, 0];
+
 const InitMap = {
     "World": {
-        "last_version": [1, 0, 0]
+        "last_version": SYSTEM_VERSION,
+        "projects": {}
     },
     "Player": {
-        //
+        "projects": {},
+        "editing_status": false
     }
 }
 
