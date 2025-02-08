9b19773acbf1d5c397ddf7ba7bc6c98a2840149e
TickPoints <tickpoints@outlook.com>
2025-02-08 15:15:38 +0800

    Fixed a bug with local tools.

--- scripts/text/local.js
+++ scripts/text/local.js
@@ -2,11 +2,11 @@ function translate(text, ...withData) {
     if (withData) {
         for (var i = 0; i < withData.length; i++) {
             if (typeof withData[i] === "string") continue;
-            if (withData[i] === undefined) {
+            if (withData[i] == undefined) {
                 withData[i] = "";
                 continue;
             }
-            withData[i] = withData[i].valueof ? withData[i].valueof() : withData.toString();
+            withData[i] = withData[i].valueOf ? withData[i].valueOf() : withData.toString();
         }
     }
     return {
