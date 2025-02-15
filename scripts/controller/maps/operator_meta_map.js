2bab5ff61af361fd614ee426657baaf0a81d8994
TickPoints <tickpoints@outlook.com>
2025-02-15 10:57:22 +0800

    Updated operator meta map.

--- scripts/controller/maps/operator_meta_map.js
+++ scripts/controller/maps/operator_meta_map.js
@@ -1,3 +1,19 @@
+function parseRGB(text) {
+    let rgb = [];
+    if (text[0] === "#") {
+        const parts = text.substring(1).match(/.{1,2}/g);
+        for (const prat of parts) rgb.push(parseInt(prat, 16));
+    } else {
+        const parts = text.split(/,\s?/);
+        for (const prat of parts) rgb.push(parseInt(prat));
+    }
+    return {
+        red: rgb[0],
+        green: rgb[1],
+        blue: rgb[2]
+    };
+}
+
 export const operator_meta_map = {
     "camera-clear": {
         ui: [
@@ -7,8 +23,67 @@ export const operator_meta_map = {
                 "Null"
             ]
         ],
-        func: function (player, result) {},
-        checker: []
+        func: function () {}
     },
-    "camera-fade": {}
+    "camera-fade": {
+        ui: [
+            [
+                "toggle",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.use_color_toggle.name",
+                false
+            ],
+            [
+                "textField",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.color_input.name",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.color_input.placeholder"
+            ]
+        ],
+        func: function (player, result, frame) {
+            const fadeCameraOptions = {};
+            if (result[0]) {
+                fadeCameraOptions.fadeColor = parseRGB(result[1]);
+            }
+            frame.args[0] = fadeCameraOptions;
+        }
+    },
+    "inputpermission-movement": {
+        ui: [
+            [
+                "textField",
+                "This is an operator with no parameters and no content.",
+                "Null"
+            ]
+        ],
+        func: function (player, result) {}
+    },
+    "inputpermission-camera": {
+        ui: [
+            [
+                "textField",
+                "This is an operator with no parameters and no content.",
+                "Null"
+            ]
+        ],
+        func: function (player, result) {}
+    },
+    "player-runMinecraftCommand": {
+        ui: [
+            [
+                "textField",
+                "This is an operator with no parameters and no content.",
+                "Null"
+            ]
+        ],
+        func: function (player, result) {}
+    },
+    "time-waitTicks": {
+        ui: [
+            [
+                "textField",
+                "This is an operator with no parameters and no content.",
+                "Null"
+            ]
+        ],
+        func: function (player, result) {}
+    }
 };
