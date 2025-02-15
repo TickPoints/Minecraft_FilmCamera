672c6b5a7a24efd04e9a76e2481f24791c001142
TickPoints <tickpoints@outlook.com>
2025-02-15 12:03:44 +0800

    Modified the parsing of RGB rule.Fixed camera-fade.

--- scripts/controller/maps/operator_meta_map.js
+++ scripts/controller/maps/operator_meta_map.js
@@ -1,17 +1,39 @@
 function parseRGB(text) {
     let rgb = [];
-    if (text[0] === "#") {
-        const parts = text.substring(1).match(/.{1,2}/g);
-        for (const prat of parts) rgb.push(parseInt(prat, 16));
-    } else {
-        const parts = text.split(/,\s?/);
-        for (const prat of parts) rgb.push(parseInt(prat));
+    try {
+        if (text[0] === "#") {
+            const parts = text.substring(1).match(/.{1,2}/g);
+            for (const prat of parts) rgb.push(parseInt(prat, 16));
+        } else {
+            const parts = text.split(/,\s?/);
+            for (const prat of parts) rgb.push(parseInt(prat));
+        }
+    } catch {
+        rgb = [255, 255, 255];
+    }
+    for (const i of rgb) {
+        if (i === NaN || i === Infinity || i === -Infinity) {
+            rgb = [255, 255, 255];
+            break;
+        }
+        if (i < 0 || i > 255) {
+            rgb = [255, 255, 255];
+            break;
+        }
     }
     return {
         red: rgb[0],
         green: rgb[1],
         blue: rgb[2]
     };
+}
+
+function parseTime(text) {
+    let time = parseInt(text);
+    if (time === NaN || time === Infinity || time === -Infinity || time < 1) {
+        time = 5;
+    }
+    return time;
 }
 
 export const operator_meta_map = {
@@ -36,12 +58,40 @@ export const operator_meta_map = {
                 "textField",
                 "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.color_input.name",
                 "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.color_input.placeholder"
+            ],
+            ["#divider"],
+            [
+                "toggle",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.use_time_toggle.name",
+                false
+            ],
+            [
+                "textField",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.time_input.fadeInTime",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.time_input.placeholder"
+            ],
+            [
+                "textField",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.time_input.fadeOutTime",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.time_input.placeholder"
+            ],
+            [
+                "textField",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.time_input.holdTime",
+                "$filmcamera.scripts.ui.new.editor_working_edit_args.meta.camera-fade.time_input.placeholder"
             ]
         ],
         func: function (player, result, frame) {
             const fadeCameraOptions = {};
             if (result[0]) {
                 fadeCameraOptions.fadeColor = parseRGB(result[1]);
+            }
+            if (result[2]) {
+                fadeCameraOptions.fadeTime = {
+                    fadeInTime: parseTime(result[3]),
+                    fadeOutTime: parseTime(result[4]),
+                    holdTime: parseTime(result[5])
+                };
             }
             frame.args[0] = fadeCameraOptions;
         }
